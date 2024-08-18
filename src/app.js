ab0bc3ac4ac29f585e94ab8757fdd6120860a90a
use63 <suratonline95@gmail.com>
2024-08-19 01:50:57 +0700

    memisahkan routes menjadi per file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 // Mengimport modul yang dibutuhkan
 import express from "express";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { setCookie } from "./utils/index.js";
-import { logger, validateRegistration } from "./middleware/index.js";
-import CryptoJS from "crypto-js";
-import { UserModel } from "./models/index.js";
-
-// Konfigurasi dotenv
-dotenv.config();
+import { logger, validateRegistration, verifyJwtToken } from "./middleware/index.js";
+import { post, get } from "./routes/index.js";
+import { PORT } from "./config/config.js"
 
 // Inisialisasi express
 const app = express();
-
-// Kunci rahasia dari variabel lingkungan
-const SECRET_KEY = process.env.SECRET_KEY || "secret key!";
-const SSL = process.env.SSL === "true";
-const PORT = process.env.PORT || 3000;
-const STATIC_SALT_CSRF = process.env.STATIC_SALT_CSRF || "some key";
-
 
 // Middleware
 app.use(express.json());
@@ -27,181 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(logger("http://localhost:3000"));
 
-// Fungsi untuk menghasilkan token JWT
-const generateJwtToken = (username) => {
-	return jwt.sign({ username }, SECRET_KEY, {
-		expiresIn: "1h",
-	});
-};
-
-// Fungsi untuk mengatur cookie
-const setJwtCookies = (res, token) => {
-	const token_jwt_base64 = Buffer.from(token).toString("base64");
-	const token_jwt_base64_a = token_jwt_base64.substring(0, 10);
-	const token_jwt_base64_b = token_jwt_base64.substring(10);
-
-    // Mengatur cookie
-	setCookie(res, [
-		{
-			key: "token_jwt_base64_a",
-			value: token_jwt_base64_a,
-			httpOnly: true,
-			secure: SSL,
-			sameSite: "Strict",
-		},
-		{
-			key: "token_jwt_base64_b",
-			value: token_jwt_base64_b,
-			httpOnly: true,
-			secure: SSL,
-			sameSite: "Strict",
-		},
-	]);
-};
-
-// Middleware untuk memeriksa token JWT
-const verifyJwtToken = (req, res, next) => {
-    // Mengambil cookie
-	const token_a = req.cookies.token_jwt_base64_a;
-	const token_b = req.cookies.token_jwt_base64_b;
-
-    // Cek apakah token ada
-	if (!token_a || !token_b) {
-		return res.status(401).send({ message: "No token provided" });
-    }
-
-    // Verifikasi token JWT
-    try {
-        // Menggabungkan token dan decode base64
-        const token = Buffer.from(token_a + token_b, "base64").toString("utf8");
-
-        // Mengecek apakah token valid
-		const decoded = jwt.verify(token, SECRET_KEY);
-
-        req.user = decoded;
-		next();
-    }
-    
-    // Jika token tidak valid
-    catch (err) {
-		res.status(401).send({ message: "Invalid token" });
-	}
-};
-
 
 // Halaman utama
-app.get("/", (req, res) => {
-    res.send(`
-        <h1>We provide API service</h1>
-        <pre>
-            get  | /api/v1/login
-            post | /api/v1/register
-            post | /api/v1/logout
-            post | /api/v1/check-cookies
-            post | /api/v1/logger
-        </pre>
-        `);
-});
+app.get("/", get.home);
 
 // Endpoint cek cookie
-app.post("/api/v1/check-cookies", verifyJwtToken, (req, res) => {
-    res.send({
-        username: req.user.username,
-    });
-});
+app.post("/api/v1/check-cookies", verifyJwtToken, );
 
 // Endpoint login
-app.post("/api/v1/login", (req, res) => {
-    // Mengambil username dan password
-    const { email, password } = req.body;
-    
-    // Cek apakah username dan password valid
-    if (email === "a" && password === "a*") {
-        const token_jwt = generateJwtToken(username);
-
-        // Mengatur cookie
-        setJwtCookies(res, token_jwt);
-
-        // Mengembalikan respons
-        res.send({ message: "Login success" });
-    } else {
-        // Jika username dan password tidak valid
-        res.status(401).send({ message: "Invalid credentials", error: true });
-	}
-});
+app.post("/api/v1/login", post.login)
 
 // Endpoint register
-app.post("/api/v1/register", validateRegistration, async (req, res) => {
-
-    // Mengambil username dan password
-    const { email, password } = req.body;
-
-    UserModel.sync();
-    
-    try {
-        const user = await UserModel.create({
-            email: email,
-            password: password
-        })
-        // Mengembalikan respons
-        res.send({ message: "Registration success", user: user });
-    } catch (err) {
-        // Jika username sudah ada
-        res.status(400).send({ message: "Email already registered.", error: true });
-    }
-})
+app.post("/api/v1/register", validateRegistration, post.register)
 
 // Endpoint logout
-app.post("/api/v1/logout", (req, res) => {
-    // Menghapus cookie
-    setCookie(res, [
-        {
-            key: "token_jwt_base64_a",
-            value: "",
-            httpOnly: true,
-            secure: SSL,
-            sameSite: "Strict",
-            maxAge: 0, // Mengatur cookie untuk kadaluarsa segera
-        },
-        {
-            key: "token_jwt_base64_b",
-            value: "",
-            httpOnly: true,
-            secure: SSL,
-            sameSite: "Strict",
-            maxAge: 0, // Mengatur cookie untuk kadaluarsa segera
-        },
-    ]);
-    res.send({ message: "Logout successful" });
-});
+app.post("/api/v1/logout", post.logout);
 
-app.post("/api/v1/logger", (req, res) => {
-    console.log("\n" + JSON.parse(req.body.log).date);
-    console.log(req.body.log);
-    res.send("success write log");
-}); 
+// Endpoint logger
+app.post("/api/v1/logger", post.logger);
 
-app.post("/api/v1/create-one-time-token", (req, res) => {
-    const key = req.body.key;
+app.post("/api/v1/create-one-time-token", post.createOneTimeToken)
 
-    const createOneTimeToken = (key, STATIC_SALT_CSRF) => {
-        const key_0 = key + Date.now();
-        let hash = CryptoJS.SHA256(key_0).toString(CryptoJS.enc.Hex);
-        const key_1 = key_0 + STATIC_SALT_CSRF;
-        hash = CryptoJS.SHA256(key_1).toString(CryptoJS.enc.Hex);
-        return hash;
-    }
 
-    const addKeyAndTokenToDatabase = async (key, hash) => {
-        // Menambahkan key dan token ke database
-    }
-
-    const hash = createOneTimeToken(key, STATIC_SALT_CSRF);
-    
-    addKeyAndTokenToDatabase(key, hash);
-
-    res.send({"key": key, "hash": hash})
-})
 
 // Menjalankan server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
